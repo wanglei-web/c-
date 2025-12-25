8d39a503d4e24a7308decdd5ca89da2203f2db8c
1728469877 <1728469877@qq.com>
2025-12-25 21:27:51 +0800

    提交一版

--- 1.cpp
+++ 1.cpp
@@ -1,16 +1,6 @@
 #include <iostream>
 using namespace std;
-class Filterclass{
-    public: 
-        Filterclass(double filtercient) :alpha(filtercient),lastfiltervalue(0.0){}
-        double filter(double input){
-            double value=(1-alpha)*input+alpha*lastfiltervalue;
-            lastfiltervalue=value;
-            return value;
-        }
-    private:
-        double alpha;
-        double lastfiltervalue;
+
     
  };
 
